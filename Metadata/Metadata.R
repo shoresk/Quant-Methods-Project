826accdbf5607ca293f1ec7b1f66d130d7a68a19
Sarah Kate Shore <shoresk@g.cofc.edu>
2016-04-15 09:49:27 -0400

    Deleted nitrate data, Added Metadata file, added Silica dataset to DMSP Predictors

---
+++ Metadata/Metadata.R
@@ -0,0 +1,20 @@
+###Metadata
+
+#19-Hex correlated pigments. 19-Hex found in Prymnesiophytes, several dinoflagellates
+#Since Peridinin is not correlated to 19-Hex, looks like presence of 19-Hex is due to prymnesiophytes
+
+Chl.c3. . (Some prymnesiophytes, diatoms and dinoflagellates)
+MgDVP   * (prasinophytes)
+Chl.c2  *** (Most diatoms, dinoflagellates, prymnesiophytes, raphidophytes, cryptophytes)
+Fuco    *** (Diatoms, prymnesiophytes, chrysiophytes, raphidophytes, several dinoflagellates)
+Neo     . (Green algae, chlorophytes, prasinophytes, euglenophytes)
+Prasino ** (Some prasinophytes)
+Allo    * (Cyanophytes, rhodophytes)
+Zeax.   ** (Cyanophytes, prochlorophytes, rhodophytes, chlorophytes)
+Croco   ** (Cryptophytes)
+Chl.b.  ** (Green algae, chlorophytes, prasinophytes, euglenophytes)
+DVChla  ** (Prochlorophytes)
+Chl     . (All phytoplankton)
+Ph.tin  * ()
+a.Car.  ** ()
+b.Car   .
