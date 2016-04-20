e8f0de6acee7f5cc1c626e59c76117136fc1e84b
Sarah Kate Shore <shoresk@g.cofc.edu>
2016-04-20 00:41:09 -0400

    Moved things to figs folder, added some plots to DMSP Predictors, Added silica analysis

--- Metadata/Metadata.R
+++ Metadata/Metadata.R
@@ -18,3 +18,27 @@ Chl     . (All phytoplankton)
 Ph.tin  * ()
 a.Car.  ** ()
 b.Car   .
+
+# Site-by-species matrix with pigments (indirect ordination) 
+
+#Drop columns that contain inessential predictors
+fields_to_drop_2= c('id', 'Station.y', 'Station.x.1'
+                   'X19.Hex.like.keto.Amount', 'Bot.Depth.m', 'Nisken.no.y', "Latitude", 
+                   'Longitude', 'Depth', 'StrainGaugePressure', 'Conductivity', 'PotentialTemp.',
+                   'Density', 'Sigmatheta', 'PAR.Irradiance', 'Nisken.no.x.1', 'HPLCSet.no', 'VialName', 
+                   'Extraction.Date', 'Vol.Filtered.L', 'Nisken.no.y.1', 'Depth.m', 'Bot.depth.m', 
+                   'Lat.degN', 'Long.degE', 'DMS.nmol.L.1', 'DMSPd.nmolL.1', 'DMSPp.nmol..L.1', 'Salinity',
+                   'Temperature', 'Oxygen', 'Oxygen.1', 'ISUS', 'Nitrate', 'Nisken.no.x.1')
+Site_by_pigs = DMSP_CTD[ , !(names(DMSP_CTD) %in% fields_to_drop_2)]
+rows_to_keep_2 = which(apply(Site_by_pigs, 1, function(x) !any(is.na(x))))
+Site_by_pigs = Site_by_pigs[rows_to_keep_2, ]
+View(Site_by_pigs)
+
+library(vegan)
+
+pca = rda(scale(Site_by_pigs))
+pca
+summary(pca)
+plot(pca, display='sp', choices = c(1,2))
+
+
--- Scripts/Silica distribution.R
+++ Scripts/Silica distribution.R
@@ -7,6 +7,24 @@ plot(Si_ALL)
 
 #Variance with Salinity
 plot(DMSP_CTD_vars$Absorbance ~ DMSP_CTD_vars$Salinity)
+#Prefers to be in 36.5 ppm range
 
-### Why doesn't it consider Absorbance a worthy field to investigate?
+#Variance wtih Temperature
+plot(DMSP_CTD_vars$Absorbance ~ DMSP_CTD_vars$Temperature)
+#28 degrees C seems to be the prefered temp
+
+#Variance with depth 
+plot(DMSP_CTD_vars$Absorbance ~ DMSP_CTD_vars$Depth, col="420")
+par(new = T)
+plot(DMSP_CTD_vars$Monad.Amount ~ DMSP_CTD_vars$Depth, col = "66")
+par(new = F)
+
+#Variance with latitude
+plot(DMSP_CTD_vars$Absorbance ~ DMSP_CTD_vars$Lat.degN)
+
+#Variance with longitude
+plot(DMSP_CTD_vars$Absorbance ~ DMSP_CTD_vars$Long.degE)
+
+#DMS vs. Silica
+DMS_Si = plot(DMSP_CTD_vars$DMS.nmol.L.1 ~ DMSP_CTD_vars$Absorbance)
 
